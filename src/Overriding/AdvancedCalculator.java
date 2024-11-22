45da3172f2a776341fbbfa7c50214d09ca5642bc
IT_Qaqash <rasifmirzayev406@gmail.com>
2024-11-22 14:00:33 +0400

    Delete src/Overriding directory
    Delete message

--- src/Overriding/AdvancedCalculator.java
+++
@@ -1,8 +0,0 @@
-package Overriding;
-
- class AdvancedCalculator extends Calculator {
-    public int add(int a , int b) {
-        System.out.println("Tam ədədlərin əlavə edilməsi üçün AdvancedCalculator-dan istifadə.");
-        return super.add(a,b);
-    }
-}
